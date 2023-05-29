235c368c03ea90414e9ef08392d2b1896aff0774
Joe Molina <joedodaniljr123@gmail.com>
2023-05-29 16:19:52 -0400

    nuevo diseno, ahora puedes escoger por tipo

--- app/app.js
+++ app/app.js
@@ -9,59 +9,93 @@ const d = document,
     
     const getPokemon2 = async () => {
     try {
-        let res = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=100"),
-        json = await res.data.results;
-        //console.log(json)
-        // Enviamos todas las solicitudes al mismo tiempo y esperamos a que se resuelvan
-        //console.log(json)
-        const requests = json.map(el => axios.get(`${el.url}`));
-        //console.log(requests)
-        const responses = await Promise.all(requests);
-        //console.log(responses)
-        const data = responses.map(res2 => res2.data);
-        //console.log(data)
+
+                let res = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151"),
+                json = await res.data.results;
         
-        // Iteramos sobre los datos para crear los elementos del fragmento
-        data.forEach(pokemon => {
+                // Enviamos todas las solicitudes al mismo tiempo y esperamos a que se resuelvan
+                const requests = json.map(el => axios.get(`${el.url}`));
+                const responses = await Promise.all(requests);
+                const data = responses.map(res2 => res2.data);
+        
+                data.forEach(pokemon => {
+                    const nombrePoke = pokemon.name;
+                    const imgPoke = pokemon.sprites.front_default;
+                    
+                    $template.querySelector('.name').textContent = nombrePoke;
+                    $template.querySelector('img').src = imgPoke;
+                    $template.querySelector("div").classList.add('pokemon-article');
+                    let $clone = d.importNode($template, true);
+                    $fragment.appendChild($clone);
+
+                })
+                // Añadimos el fragmento a la tabla
+                $section.appendChild($fragment);
+
+                d.querySelectorAll('.buscar-tipo button').forEach(el =>{
+                    el.disabled = false;
+                })
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        
+    d.addEventListener('DOMContentLoaded', getPokemon2);
+        
+        
+    window.addEventListener('click', async e => {
+            
+        if(e.target.closest('.pokemon-article')){
+            e.preventDefault()
+            const $pokemon = e.target.closest('.pokemon-article');
+    
+            console.log($pokemon.querySelector('p').textContent)
+            }   
+    
+        d.querySelectorAll('.buscar-tipo button').forEach(el =>{
+            const type = el.textContent;
+
+            if(e.target.closest(`.type-${type}`)){
+                d.querySelectorAll('.pokemon-article').forEach( pokemon =>{
+                    pokemon.remove()
+                })
+                burcarPorTipo(type)
+            }
+        })
+
+})
+
+
+
+    const burcarPorTipo = async (tipo) =>{
+        d.querySelectorAll('.buscar-tipo button').forEach(el =>{
+            el.disabled = true
+        })
+        let respuestaTipos = await axios.get(`https://pokeapi.co/api/v2/type/${tipo}`),
+            jsonTipos = respuestaTipos.data.pokemon;
+
+        console.log(jsonTipos)
+        
+        const requestTipos = jsonTipos.map(el => axios.get(`${el.pokemon.url}`)),
+            responsesTipos = await Promise.all(requestTipos),
+            dataTipos = responsesTipos.map(responsesTipos => responsesTipos.data)
+        console.log(dataTipos)
+
+        dataTipos.forEach(pokemon => {
             const nombrePoke = pokemon.name;
-            const imgPoke = pokemon.sprites.front_default;
+            const imgPoke = pokemon.sprites.front_default || "" ;
+
             
             $template.querySelector('.name').textContent = nombrePoke;
             $template.querySelector('img').src = imgPoke;
-            $template.querySelector("a").classList.add('pokemon-article');
+            $template.querySelector("div").classList.add('pokemon-article');
             let $clone = d.importNode($template, true);
             $fragment.appendChild($clone);
+
         })
-        
-        // Añadimos el fragmento a la tabla
         $section.appendChild($fragment);
-        sexo()
-    } catch (err) {
-        console.error(err);
-    }
-};
 
-d.addEventListener('DOMContentLoaded', getPokemon2);
-
-
-const sexo = async () =>{
-    $section.querySelectorAll('.pokemon-article').forEach(el => {
-
-        el.addEventListener('click', async e =>{
-
-            console.log(`${el.querySelector('p').textContent}`)
-
-            let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${el.querySelector('p').textContent}`),
-            json3 = await response.data;
-                
-            console.log(json3.sprites.other["official-artwork"].front_default)
-                
-            d.querySelector('.pokemon-nombre').textContent = `name: ${json3.name}`;
-            d.querySelector('.pokemon-tipo').textContent = `type: ${json3.types[0].type.name}`;
-            d.querySelector('.pokemon-img').src = json3.sprites.other["official-artwork"].front_default;
-            
-
-            })
+        d.querySelectorAll('.buscar-tipo button').forEach(el =>{
+            el.disabled = false;
         })
-}
-
+    }
